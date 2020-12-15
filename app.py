18e26d1ec544a06d1b58d204f966c855057b42e7
Peter B <5107405+pmbaumgartner@users.noreply.github.com>
2020-12-15 13:31:56 -0500

    Update app with readme and improved interface

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-import streamlit as st
-import gensim
 from pathlib import Path
 
+import gensim
+import pandas as pd
+import streamlit as st
 
 models = {
     "Lowercase Unigram": "models/lowercase-sg.w2v.kv",
@@ -11,26 +12,57 @@ models = {
 
 assert all((Path(p).exists() for p in models.values())), "Model Files Missing"
 
+readme = Path("readme.md").read_text()
+
 
 @st.cache(hash_funcs={gensim.models.keyedvectors.Word2VecKeyedVectors: lambda _: None})
 def load_model(name):
     path = models[name]
     model = gensim.models.KeyedVectors.load(path, mmap="r")
-    model.vectors_norm = model.vectors  # prevent recalc of normed vectors
     model.most_similar("opioid")  # any word will do: just to page all in
     return model
 
 
 st.markdown("# Reddit Opioid Terminology - Word Embedding Explorer")
+st.markdown(
+    "To use, select a model, type in a set of terms (optionally separated by commas), and select the top n number of items to return. For details on model specifics, expand the details below"
+)
 
+with st.beta_expander("Expand for Model Details"):
+    st.markdown(readme)
+
+st.markdown("---")
+st.markdown("**Model Selection + Inputs**")
 model_select = st.selectbox("Model", options=list(models.keys()))
 selected_model = load_model(model_select)
 
 terms = st.text_input("Terms (comma separated)", "oxycontin")
 input_terms = [term.strip().lower() for term in terms.split(",")]
+
 if model_select == "Lowercase scispaCy Bigrams":
     input_terms = [term.replace(" ", "__") for term in input_terms]
 
-candidate_terms = selected_model.most_similar(input_terms, topn=10)
+vocab_terms = [t for t in input_terms if t in selected_model.vocab]
+oov_terms = [t for t in input_terms if t not in selected_model.vocab]
 
-st.write(candidate_terms)
+
+topn = st.select_slider("Return Top N=", options=[25, 50, 100, 200])
+candidate_terms = selected_model.most_similar(vocab_terms, topn=topn)
+
+if oov_terms:
+    oov_str = ", ".join(oov_terms)
+    st.warning(f"Input Terms Not in Model Vocabulary: {oov_str}")
+
+st.markdown("---")
+
+if model_select == "Lowercase scispCy Bigrams":
+    candidate_terms = [(term.replace("__", " "), sim) for term, sim in candidate_terms]
+
+in_vocab_str = ", ".join(vocab_terms)
+st.markdown(
+    f"Top _{topn}_ most similar terms for **{in_vocab_str}** with _{model_select}_."
+)
+term_df = pd.DataFrame(candidate_terms, columns=["Term", "Similarity"])
+if model_select == "Lowercase scispCy Bigrams":
+    term_df["Term"] = term_df["Term"].str.replace("__", " ")
+st.table(term_df)
---
+++ readme.md
@@ -0,0 +1,31 @@
+## Reddit Opioid-Related word2vec Word Embedding Models
+
+This repository contains 3 word2vec word embedding models that were used to better understand vocabulary around opioid use. 
+
+### Data
+
+Data was collected from two subreddits: `r/opiates` and `r/opiatesrecovery`. The model was trained using three types of content from the two subreddits: post titles, post selftexts, and all comments. The table below describes the number of documents per source.
+
+| Content Type   | r/Opiates | r/OpiatesRecovery |
+| -------------- | --------- | ----------------- |
+| Post Titles    | 146046    | 14503             |
+| Post Selftexts | 103970    | 12238             |
+| Comments       | 3162152   | 277387            |
+
+### Preprocessing
+
+All content was transformed to lowercase and tokenized with spaCy or scispaCy.
+
+For the bigram model, [gensim's Phrase tool](https://radimrehurek.com/gensim_3.8.3/models/phrases.html) was used for collocation detection, with a min_count of 50 and a threshold of 10 using the PMI scorer. Bigrams are separated in the vocabulary by a double underscore: `__`
+
+### Model Info
+
+Three models were trained with [gensim's Word2Vec](https://radimrehurek.com/gensim_3.8.3/models/word2vec.html) using the same parameter set:
+
+```min_count=5, size=100, window=5, iter=5, sg=1, seed=1234```
+
+| Model Name                 | Vocab Size           |
+| -------------------------- | -------------------- |
+| Lowercase Unigram          | 85777                |
+| Lowercase scispaCy Unigram | 85489                |
+| Lowercase scispaCy Bigrams | 90935 (5518 bigrams) |
